19aa0963c150c8844dc352f5ce9777ac77da1f5e
Darrian Bagley <dbagle14@gmail.com>
2022-03-23 02:11:12 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    return Math.abs(42 - location);
+}
+
+function distanceFromHqInFeet(location) {
+    return distanceFromHqInBlocks(location) * 264;
+}
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+    return Math.abs(startBlock - endBlock) * 264;
+}
+
+function calculatesFarePrice(startBlock, endBlock) {
+    let distance = distanceTravelledInFeet(startBlock, endBlock);
+    if (distance > 2500) {
+        return "cannot travel that far";
+    } else if (distance > 2000) {
+        return 25;
+    } else if (distance - 400 > 0) {
+        return (distance - 400) * 0.02;
+    } else {
+        return 0;
+    }
+}
